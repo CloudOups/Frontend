97a695691ab56577c3514a343510dee14682b1c7
michel <michel.bodoldjob@esprit.tn>
2024-04-21 21:21:39 +0100

    Résolution des conflits dans les fichiers de configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,24 @@ import { HomeComponent } from './home/home.component';
 import { BlogComponent } from './blog/blog.component';
 import { BlogdetailsComponent } from './blogdetails/blogdetails.component';
 import { LoginComponent } from './login/login.component';
+<<<<<<< Updated upstream
+=======
+//-------------------hedi---------------------------------------
+import { ListBlogComponent } from './Blog/list-blog/list-blog.component';
+import { ListBlogbackComponent } from './Blog/list-blogback/list-blogback.component';
+import { UpdateBlogComponent } from './Blog/update-blog/update-blog.component';
+import { AddBlogComponent } from './Blog/add-blog/add-blog.component'
+import { BlogDetailsComponent } from './Blog/blog-details/blog-details.component';
+import { ListProduitComponent } from './produit/list-produit/list-produit.component';
+import { DetailsProduitComponent } from './produit/details-produit/details-produit.component';
+// import {  } from '';
+// import {  } from '';
+// import {  } from '';
+// import {  } from '';
+// import {  } from '';
+// import {  } from '';
+
+>>>>>>> Stashed changes
 
 
 const routes: Routes = [
@@ -15,6 +33,28 @@ const routes: Routes = [
 
 
 
+<<<<<<< Updated upstream
+=======
+  // ------------------------------------------paths hedi-------------------------------------
+  { path :'listblog', component: ListBlogComponent},
+  { path :'blogdetails/:id', component: BlogDetailsComponent},
+  { path :'addblog', component: AddBlogComponent},
+  { path :'updateBlog/:id', component: UpdateBlogComponent},
+  { path :'listBlogback', component: ListBlogbackComponent},
+  // { path :'', component: },
+  // { path :'', component: },
+  // { path :'', component: },
+  // { path :'', component: },
+  // { path :'', component: },
+  // { path :'', component: },
+  
+
+    //---------------------------------------------paths michel-------------------------------------
+
+
+    { path : 'produit', component: ListProduitComponent},
+    { path : 'detailProduit/:id', component: DetailsProduitComponent},
+>>>>>>> Stashed changes
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,31 @@ import { BlogComponent } from './blog/blog.component';
 import { BlogdetailsComponent } from './blogdetails/blogdetails.component';
 import { NavbarconnectedComponent } from './navbarconnected/navbarconnected.component';
 import { LoginComponent } from './login/login.component';
+<<<<<<< Updated upstream
 import { CommentsComponent } from './comments/comments.component';
 import { HomeComponent } from './home/home.component';
+=======
+import { AddCommentaireComponent } from './Commentaire/add-commentaire/add-commentaire.component';
+import { HomeComponent } from './shared/home/home.component';
+import { UpdateBlogComponent } from './Blog/update-blog/update-blog.component';
+import { SidemenuComponent } from './dashboard/sidemenu/sidemenu.component';
+import { TableComponent } from './dashboard/table/table.component';
+import { CalendrierEventsComponent } from './dashboard/calendrier-events/calendrier-events.component';
+import { StaticsCircleComponent } from './dashboard/statics-circle/statics-circle.component';
+import { StaticsBarsVComponent } from './dashboard/statics-bars-v/statics-bars-v.component';
+import { StaticsBarsHComponent } from './dashboard/statics-bars-h/statics-bars-h.component';
+import { Form2Component } from './dashboard/form2/form2.component';
+import { FormVerticaleComponent } from './dashboard/form-verticale/form-verticale.component';
+import { FormHorizontalComponent } from './dashboard/form-horizontal/form-horizontal.component';
+import { RegisterComponent } from './register/register.component';
+import { ListBlogbackComponent } from './Blog/list-blogback/list-blogback.component';
+import { ListBlogComponent } from './Blog/list-blog/list-blog.component';
+import { ListProduitComponent } from './produit/list-produit/list-produit.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StatusPanierComponent } from './produit/status-panier/status-panier.component';
+import { DetailsProduitComponent } from './produit/details-produit/details-produit.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -29,12 +52,41 @@ import { HomeComponent } from './home/home.component';
     BlogdetailsComponent,
     NavbarconnectedComponent,
     LoginComponent,
+<<<<<<< Updated upstream
     CommentsComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+=======
+    AddCommentaireComponent,
+    HomeComponent,
+    UpdateBlogComponent,
+    SidemenuComponent,
+    TableComponent,
+    CalendrierEventsComponent,
+    StaticsCircleComponent,
+    StaticsBarsVComponent,
+    StaticsBarsHComponent,
+    Form2Component,
+    FormVerticaleComponent,
+    FormHorizontalComponent,
+    RegisterComponent,
+    ListBlogbackComponent,
+    ListBlogComponent,
+    ListProduitComponent,
+    StatusPanierComponent,
+    DetailsProduitComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
+    FontAwesomeModule,
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
