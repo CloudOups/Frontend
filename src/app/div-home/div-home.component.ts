32b8654ee3123e20d8a69e91cb3f44291f704d59
yassinChaa <yassin.chaabouni@esprit.tn>
2024-05-05 12:07:06 +0100

    taslih

--- src/app/div-home/div-home.component.ts
+++ src/app/div-home/div-home.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { TypeTerrain } from '../Models/Terrain/typeTerrain';
+import { Terrain } from '../Models/Terrain/terrain';
+import { TerrainService } from '../services/terrain.service';
 @Component({
-  selector: 'app-div-home',
-  templateUrl: './div-home.component.html',
-  styleUrls: ['./div-home.component.css']
-})
-export class DivHomeComponent {
+  selector: 'app-div-home',})
+  export class DivHomeComponent {
 
-}
+    startTime!: string; // Property to store selected start time
+    endTime!: string;   // Property to store selected end time
+    selectedTypeTerrain!: TypeTerrain;
+    constructor(private router: Router,private terrainService: TerrainService) {}
+    navigateToReservation() {
+      if (this.startTime!=null&& this.endTime&&this.selectedTypeTerrain) {
+      } else {
+        console.error("Please select both start and end times.");
+    }}
+    
+    typeTerrainOptions = Object.values(TypeTerrain); // Array of enum values
+    terrains!: Terrain []
+    
+    
+    }
+    
